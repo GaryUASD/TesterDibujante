56f828820d3a634154a965db3e094f13932ad76c
Jesus Daniel Valdez <>
2020-12-29 22:25:28 -0400

    Fusiona las ramas DIB-007 y DIB-030 con 'Base'

---
+++ src/figuras/TrianguloEscaleno.java
@@ -0,0 +1,43 @@
+package figuras;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
+/**
+ *
+ * @author Jesus Daniel Valdez
+ */
+public class TrianguloEscaleno extends Rectangulo {
+
+    Color colorFondo;
+    Color colorBorde;
+
+    public TrianguloEscaleno(Point ubicacion, Color colorBorde, Color colorFondo) {
+        super(ubicacion);
+        this.colorFondo = colorFondo;
+        this.colorBorde = colorBorde;
+    }
+
+    /**
+     *
+     * @param g
+     */
+    @Override
+
+    public void dibujar(Graphics g) {
+
+        Point punto1 = new Point(ubicacion.x, ubicacion.y + altura);
+        Point punto2 = new Point(ubicacion.x + anchura, ubicacion.y + altura);
+        Point punto3 = new Point(ubicacion.x - anchura / 2, ubicacion.y);
+
+        int[] puntosX = new int[]{punto1.x, punto2.x, punto3.x};
+        int[] puntosY = new int[]{punto1.y, punto2.y, punto3.y};
+
+        g.setColor(this.colorFondo);
+        g.fillPolygon(puntosX, puntosY, 3);
+        g.setColor(this.colorBorde);
+        g.drawPolygon(puntosX, puntosY, 3);
+
+    }
+}
